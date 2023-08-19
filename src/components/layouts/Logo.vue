ff47a43356378e4a8edea833a74c44adbf3b0dfe
Dhiya <adiadiadha@google.com>
2023-08-19 20:50:52 +0700

    update component navbar vid 8

---
+++ src/components/layouts/Logo.vue
@@ -0,0 +1,10 @@
+<script setup>
+import { RouterLink } from "vue-router";
+</script>
+
+<template>
+    <!-- Logo -->
+    <RouterLink to="/" class="flex items-center">
+        <img src="@/assets/img/logo.svg" class="h-8 ml-3 sm:ml-0 sm:h-8" alt="Zulkit-dha Logo" />
+    </RouterLink>
+</template>
